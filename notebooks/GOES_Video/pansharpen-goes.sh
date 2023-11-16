6ec00134a71bb7d5a4c7df1f5a8dc1b7dec6230c
ax0nic <ax0nic@protonmail.com>
2023-11-16 13:53:56 -0600

    vrt no pan shaprening

--- notebooks/GOES_Video/pansharpen-goes.sh
+++ notebooks/GOES_Video/pansharpen-goes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Script started."
+
 # Parse arguments
 while getopts i:o:v:p:sv:t option; do
     case "${option}" in
@@ -12,6 +14,13 @@ while getopts i:o:v:p:sv:t option; do
     esac
 done
 
+echo "Input directory: $INPUT_DIR"
+echo "Output directory: $OUTPUT_DIR"
+echo "VRT Bands: $VRT_BANDS"
+echo "Pan Band: $PAN_BAND"
+echo "Save VRT: $SAVE_VRT"
+echo "Save GeoTIFF: $SAVE_GEOTIFF"
+
 # Function to match files based on band and start time
 match_files() {
     local start_time=$1
@@ -19,55 +28,60 @@ match_files() {
     local matched_files=()
 
     for band in "${bands[@]}"; do
-        # Find files with matching start time and band
         local file=$(find "$INPUT_DIR" -type f -name "*M6C${band}_G16_s${start_time}*.nc")
         if [ -n "$file" ]; then
             matched_files+=("$file")
         fi
     done
 
-    echo "${matched_files[@]}"
+    printf '%s\n' "${matched_files[@]}"
 }
 
 # Function to generate VRT
 generate_vrt() {
     local start_time=$1
-    local files=($2)
+    shift # remove the first argument (start_time)
+    local files=("$@")
     local vrt_command="gdalbuildvrt -overwrite -resolution highest -separate -r cubic ${OUTPUT_DIR}/${start_time}.vrt"
 
     for file in "${files[@]}"; do
         vrt_command+=" NETCDF:\"$file\":Rad"
     done
 
-    # Pan sharpening
-    if [ -n "$PAN_BAND" ]; then
-        local pan_file=$(find "$INPUT_DIR" -type f -name "*M6C${PAN_BAND}_G16_s${start_time}*.nc")
-        if [ -n "$pan_file" ]; then
-            vrt_command+=" NETCDF:\"$pan_file\":Rad"
-        fi
-    fi
-
-    eval $vrt_command
+    echo "Executing VRT command: $vrt_command"
+    eval "$vrt_command"
 }
 
 # Function to generate GeoTIFF from VRT
 generate_geotiff() {
     local vrt_file=$1
+    echo "Converting VRT to GeoTIFF for $vrt_file"
     gdal_translate "${vrt_file}" "${vrt_file%.vrt}.tif"
 }
 
 # Main processing loop
 for file in "$INPUT_DIR"/*; do
+    echo "Processing file: $file"
     if [[ $file =~ M6C([0-9]{2})_G16_s([0-9]+)_ ]]; then
-        local start_time=${BASH_REMATCH[2]}
-        local matched_files=$(match_files "$start_time")
+        start_time=${BASH_REMATCH[2]}
+        echo "Found start time: $start_time"
+        matched_files=$(match_files "$start_time")
         if [ -n "$matched_files" ]; then
+            read -a files_array <<<$matched_files # Convert string to array
             if [ "$SAVE_VRT" = true ]; then
-                generate_vrt "$start_time" "$matched_files"
+                echo "Generating VRT for start time: $start_time"
+                generate_vrt "$start_time" "${files_array[@]}"
             fi
             if [ "$SAVE_GEOTIFF" = true ]; then
+                echo "Generating GeoTIFF for start time: $start_time"
                 generate_geotiff "${OUTPUT_DIR}/${start_time}.vrt"
             fi
+        else
+            echo "No matched files for start time: $start_time"
         fi
+    else
+        echo "File $file does not match the pattern."
     fi
 done
+
+echo "Script finished."
